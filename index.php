e9e5f12030249cb957ab97d8ecd2674f4b5d9ba3
Michal Moravec <michi.m@gmail.com>
2015-09-08 17:07:48 +0200

    Updated index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Welcome to OpenShift</title>
+  <title>ILess demo</title>
 
 <style>
 
@@ -208,7 +208,7 @@ line-height: 1.4;
 
 <section class='container'>
           <hgroup>
-            <h1>Welcome to your PHP application on OpenShift</h1>
+            <h1>Welcome to ILess demo</h1>
           </hgroup>
 
 
